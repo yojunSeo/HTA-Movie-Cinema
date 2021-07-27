6bdb99a36d3c2cc6044bc4f0bdfa3d8dd9738e4f
jiimin <970602ljm@naver.com>
2021-07-27 15:35:50 +0900

    지민: 소스트리테스트

---
+++ hmc/src/main/java/com/hmc/web/controller/RegisterController.java
@@ -0,0 +1,18 @@
+package com.hmc.web.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+   
+   private static Logger logger = LogManager.getLogger(RegisterController.class);
+   
+   @GetMapping("/register")
+   public String register() {
+      return "register/agreeRegister";
+   }
+
+}
