e197444765cfa063c25c4425d651942a2fc20c65
yojunSeo <yojun>
2021-07-27 12:32:54 +0900

    <docs> test 컨트롤러 경로 삭제

--- hmc/src/main/java/com/hmc/web/controller/test.java
+++ hmc/src/main/java/com/hmc/web/controller/test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class test {
 
-	@GetMapping(path = {"/", "/test"})
+	@GetMapping(path = {"/test"})
 	public String testHome() {
 		// 이클립스와 소스트리를 연동해봤습니다. 테스트
 		return "test/home";
