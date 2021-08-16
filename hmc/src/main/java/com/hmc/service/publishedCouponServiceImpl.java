3256ba1b44334d1e639a4612cf15367e2859e857
jiimin <970602ljm@naver.com>
2021-08-16 23:11:41 +0900

    <feat> : 지급된 쿠폰테이블 service, impl author: 이지민

---
+++ hmc/src/main/java/com/hmc/service/publishedCouponServiceImpl.java
@@ -0,0 +1,27 @@
+package com.hmc.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hmc.dao.PublishedCouponDao;
+import com.hmc.vo.PublishedCoupon;
+
+public class publishedCouponServiceImpl implements publishedCouponService {
+	
+	private PublishedCouponDao publisedCouponDao;
+	
+
+	@Override
+	public int getTotalRows(Map<String, Object> param) {
+		int rows = publisedCouponDao.getTotalRows(param);
+		return rows;
+	}
+
+
+	@Override
+	public List<PublishedCoupon> getAllCouponsByUserId(Map<String, Object> param) {
+		List<PublishedCoupon> publishedCoupon = publisedCouponDao.getAllCouponsByUserId(param);
+		return publishedCoupon;
+	}
+
+}
